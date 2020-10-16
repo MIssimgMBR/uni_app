320fd069dcccbee5907a8fb6936cad930c140635
MissingMBR <767933679@qq.com>
2020-10-16 13:25:33 +0800

    创建

---
+++ uniapp_Admin/pages/data/data.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		Data
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ uniapp_Admin/pages/report/report.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		Report
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
